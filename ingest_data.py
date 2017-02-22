307fd93ddb9669db64673c7093b94e5ed2017020
adamalloul <adampackets@gmail.com>
2017-02-22 22:21:47 +0100

    First commit

---
+++ ingest_data.py
@@ -0,0 +1,10 @@
+
+
+def handler_name(event, context):
+    """
+    :param event: contains the event data received by the API.
+      Can be dict, list, str, int, float, or NoneType type.
+    :param context: context of the Lambda environment. Can be handy
+    :return: Not sure yet :)
+    """
+    return 0
